c4643c627da9d6da2e05b6b0aeecebd95659d472
mohit77bansal <mohitbansal108@gmail.com>
2019-06-27 13:38:09 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install software-properties-common
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
