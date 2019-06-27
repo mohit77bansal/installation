c5e8ffc6a1e3599080f00e9e1dfb7b358ec068c8
mohit77bansal <mohitbansal108@gmail.com>
2019-06-27 13:39:04 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get install software-properties-common
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
