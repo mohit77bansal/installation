1524515e46201d0d60b0c0b8a3cc63585002f49f
mohit77bansal <mohitbansal108@gmail.com>
2019-07-18 15:13:05 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,3 +6,5 @@ sudo apt-get update
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
